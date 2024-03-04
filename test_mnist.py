e4bf7a167bea5752a6dec70c18c0aa6b1f43bf93
ia.novoselov <ia.novoselov@tensor.ru>
2024-03-04 16:08:00 +0500

    mnist

--- test_mnist.py
+++ test_mnist.py
@@ -6,24 +6,26 @@ from neural import ActivationFunc, DifferensiateFunc, Network
 
 (x_tarin, y_train), (x_test, y_test) = mnist.load_data()
 
-net = Network([3, 4, 1])
+# Преобразовать матрицу в массив - чтобы подавать на вход сети
+images = x_tarin[0:1000].reshape(1000, 28*28)
+labels = y_train[0:1000]
+
+one_hot_labels = np.zeros((len(labels), 10))
+
+for i, l in enumerate(labels):
+    one_hot_labels[i][l] = 1
+labels = one_hot_labels
+
+net = Network([784, 40, 10])
 net.activation_func = ActivationFunc.relu
 net.differenciate_func = DifferensiateFunc.relu
 
-data = np.array([[0, 0, 1],
-                 [0, 1, 1],
-                 [1, 0, 1],
-                 [1, 1, 1],
-                 ])
-
-expectation = np.array([0, 1, 1, 0])
-
-for iterations in range(1000):
+for iterations in range(350):
     common_error = 0
-    for i in range(len(expectation)):
+    for i in range(len(images)):
 
-        data_input = data[i]
-        goal = expectation[i]
+        data_input = images[i]
+        goal = labels[i]
 
         result = net.forward(data_input)
         error = net.back_propagation(result, goal)
